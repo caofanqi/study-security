a95280be81d1615a4055468a8dc941a3bf00e6d6
曹凡启 <caofanqi@outlook.com>
2020-01-20 15:54:53 +0800

    init

---
+++ cfq-user-api/src/main/java/cn/caofanqi/security/UserApiApplication.java
@@ -0,0 +1,18 @@
+package cn.caofanqi.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author caofanqi
+ * @date 2020/1/20 13:00
+ */
+@SpringBootApplication
+public class UserApiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApiApplication.class, args);
+    }
+
+}
---
+++ cfq-user-api/src/test/java/cn/caofanqi/security/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.caofanqi.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
